d1520261be4749e5415285e2672920f17ad304b6
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2023-04-18 10:16:30 -0600

    Crear paciente

--- src/app/pages/create-paciente/create-paciente.component.ts
+++ src/app/pages/create-paciente/create-paciente.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-paciente',
@@ -14,13 +15,28 @@ export class CreatePacienteComponent {
     apellidos: '',
     direccion: '',
     telefono: '',
-    centroMedico: ''
+    centroMedico: '',
+    estado: ''
   };
 
   centrosMedicos = ['Centro Médico A', 'Centro Médico B', 'Centro Médico C'];
+  estados = ['1', '2', '3'];
+
+  constructor(private userService: UserService) { }
 
   submitForm(formulario: NgForm) {
-    console.log(formulario.value);
-    // Aquí podrías enviar los datos del formulario al servidor
+    const params = {
+      cui: this.paciente.cui,
+      nombre: this.paciente.nombre,
+      apellidos: this.paciente.apellidos,
+      direccion: this.paciente.direccion,
+      telefono: this.paciente.telefono,
+      id_centro_medico: this.paciente.centroMedico,
+      id_estado: this.paciente.estado
+    };
+    
+    this.userService.createMedico(params).subscribe((resp: any)=>{
+      console.log(resp);
+    });
   }
 }
