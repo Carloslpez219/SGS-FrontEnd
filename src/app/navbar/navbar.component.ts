63112fefb5c7dceea72af9a73abffa583af49d0c
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2023-04-21 12:39:04 -0600

    actualizacion navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,68 @@ import { Router } from '@angular/router';
 export class NavbarComponent {
   constructor(private router: Router) { }
 
+  onActualizacionSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    switch (value) {
+      case 'actualizarMedico':
+        this.navigateToUpdateMedico();
+        break;
+      case 'actualizarPaciente':
+        this.navigateToUpdatePaciente();
+        break;
+      default:
+        break;
+    }
+  }
+
+  onBitacorasSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    switch (value) {
+      case 'verBitacoraHistorial':
+        this.navigateToBitacoraHistorial();
+        break;
+      case 'verBitacoraTraspaso':
+        this.navigateToBitacoraTraspaso();
+        break;
+      default:
+        break;
+    }
+  }
+
+  onCreacionSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    switch (value) {
+      case 'crearMedico':
+        this.navigateToCreateMedico();
+        break;
+      case 'crearPaciente':
+        this.navigateToCreatePaciente();
+        break;
+      default:
+        break;
+    }
+  }
+
+  onInventarioSelect(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    switch (value) {
+      case 'verInventarioMedicamento':
+        this.navigateToInventarioMedicamento();
+        break;
+      case 'verInventarioMateriales':
+        this.navigateToInventarioMateriales();
+        break;
+      case 'registrarInventarioMedicamento':
+        this.navigateToInsertInventarioMedicamento();
+        break;
+      case 'registrarInventarioMateriales':
+        this.navigateToInsertInventarioMateriales();
+        break;
+      default:
+        break;
+    }
+  }
+
   navigateToHome() {
     this.router.navigate(['/home']);
   }
